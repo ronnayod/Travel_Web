a4bd81df6855c41c176beea5c12b64667a180663
ronnayod <gaen172543@gmail.com>
2025-03-30 14:49:48 +0700

    up Menu Sidebar

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaUserCircle } from "react-icons/fa";
-import { link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const navbarLinks = [
   {
@@ -26,7 +26,7 @@ const ResponsiveMenu = ({showMenu, setShowMenu}) => {
     <div className={`${
       showMenu ? "left-0" : "-left-[100%]"
     } fixed bottom-0 top-0 z-0 flex h-screen w-[75%]
-    flex-col justify-between bg-white dark:bg-gray-900
+    flex-col justify-between bg-white
     dark:text-white px-8 pb-8 pt-16 text-black
     transition-all duration-200 md:hidden rounded-r-xl
     shadow-md`}
